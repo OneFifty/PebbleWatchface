618878ce02c3a759db60a3dd8186887e444a5955
OneFifty <z.alexpan@gmail.com>
2016-05-18 14:36:53 -0700

    Added position for weather

--- src/main.c
+++ src/main.c
@@ -2,7 +2,10 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
-static GFont sickass_font;
+static TextLayer *s_weather_layer;
+static GFont sick_time_font;
+static GFont sick_weather_font;
+
 
 static void update_yo_time() {
   // Get a tm structure
@@ -30,24 +33,46 @@ static void main_window_load(Window *window) {
   GRect bounds = layer_get_bounds(window_layer);
 
   // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+  s_time_layer = text_layer_create(GRect(0, 52, bounds.size.w, 150));
   
   // Load the custom font
-  sickass_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_IMAGINE_FONT_42));
+  sick_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_IMAGINE_FONT_48));
 
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, sickass_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  text_layer_set_font(s_time_layer, sick_time_font);
 
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  
+  // Create temperature Layer
+  s_weather_layer = text_layer_create(GRect(10, 25, bounds.size.w, 100));
+
+  // Style the text
+  text_layer_set_background_color(s_weather_layer, GColorClear);
+  text_layer_set_text_color(s_weather_layer, GColorBlack);
+  text_layer_set_text_alignment(s_weather_layer, GTextAlignmentLeft);
+  text_layer_set_text(s_weather_layer, "85");
+  
+  // Create second custom font, apply it and add to Window
+  sick_weather_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_IMAGINE_FONT_20));
+  text_layer_set_font(s_weather_layer, sick_weather_font);
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+
 }
 
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
+  
+  // Unload GFont
+  fonts_unload_custom_font(sick_time_font);
+  
+  // Destroy weather elements
+  text_layer_destroy(s_weather_layer);
+  fonts_unload_custom_font(sick_weather_font);
 }
 
 ////////////////////////////////////////////////////////////
